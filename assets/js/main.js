68ce4373fbc9ead374d4dc0953cd90d9b1468ab8
Danila <dvkolomiets@gmail.com>
2021-12-21 15:13:37 +0500

    Fix bugs and add new function

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,14 @@ $(function() {
     });
 
 });
+
+$(document).ready(function(){
+    $("#menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 700);
+    });
+});
+
+AOS.init();
